173b1dd44fdbd6710b333eba2e88ae886d20a6d5
GreatWolf20XX <artem.naumov.dev@gmail.com>
2025-06-08 21:00:19 +0300

    feature/add-style-guideline (#1)
    * feat: add colors
    
    * feat: set color scheme
    
    * feat: add font styles
    
    * chore: update version to v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.ui.text.google.fonts)
 }
