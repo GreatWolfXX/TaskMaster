05bc87ecf0b1cf9a2417fa81c615e62ec3c14006
GreatWolf20XX <artem.naumov.dev@gmail.com>
2025-06-13 01:42:12 +0300

    feature/onboarding-screen (#3)
    * feat: add custom button
    
    * feat: add slider indicator
    
    * refactor: unnecessary curly braces in the CustomButton file were removed
    
    * fix: removed wrapContentWidth on custom button
    
    * feat: add onboarding pages
    
    * fix: ripple shape and colors
    
    * feat: add koin and navigation 3 libraries
    
    * feat: slider indicator now use pager state
    
    * feat: add onboarding viewmodel
    
    * chore: update version to v0.1.3

---
+++ app/src/main/java/com/greatwolf/taskmaster/ui/screen/onboarding/OnboardingPages.kt
@@ -0,0 +1,35 @@
+package com.greatwolf.taskmaster.ui.screen.onboarding
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.greatwolf.taskmaster.R
+
+sealed class OnboardingPages(
+    @DrawableRes val image: Int,
+    @StringRes val title: Int,
+    @StringRes val description: Int
+) {
+    data class First(val isSystemDark: Boolean) : OnboardingPages(
+        image = if (isSystemDark) R.drawable.onboarding_1_dark else R.drawable.onboarding_1_light,
+        title = R.string.onboarding_title_1,
+        description = R.string.onboarding_desc_1
+    )
+
+    data class Second(val isSystemDark: Boolean) : OnboardingPages(
+        image = if (isSystemDark) R.drawable.onboarding_2_dark else R.drawable.onboarding_2_light,
+        title = R.string.onboarding_title_2,
+        description = R.string.onboarding_desc_2
+    )
+
+    data class Third(val isSystemDark: Boolean) : OnboardingPages(
+        image = if (isSystemDark) R.drawable.onboarding_3_dark else R.drawable.onboarding_3_light,
+        title = R.string.onboarding_title_3,
+        description = R.string.onboarding_desc_3
+    )
+
+    data object Fourth : OnboardingPages(
+        image = R.drawable.onboarding_4,
+        title = R.string.onboarding_title_4,
+        description = R.string.onboarding_desc_4
+    )
+}
