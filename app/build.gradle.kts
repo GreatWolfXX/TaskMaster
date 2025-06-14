1b57c62f51319a6937785178d19f534bf04eed8a
Artem <artem.naumov.dev@gmail.com>
2025-06-14 23:01:09 +0300

    chore: update version to v0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.greatwolf.taskmaster"
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
