015dc805a85714d60a146dbc0a2957608d29ea8c
Artem <artem.naumov.dev@gmail.com>
2025-06-17 16:27:17 +0300

    feat: add catching error in DataStoreSettingsStore

--- core/datastore/src/main/java/com/greatwolf/datastore/DataStoreSettingsStore.kt
+++ core/datastore/src/main/java/com/greatwolf/datastore/DataStoreSettingsStore.kt
@@ -2,10 +2,13 @@ package com.greatwolf.datastore
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.IOException
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
@@ -20,8 +23,16 @@ class DataStoreSettingsStore(context: Context) {
     }
 
     fun <T> readState(key: Preferences.Key<T>, defaultValue: T): Flow<T> {
-        return dataStore.data.map { settings ->
-            settings[key] ?: defaultValue
-        }
+        return dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { settings ->
+                settings[key] ?: defaultValue
+            }
     }
 }
