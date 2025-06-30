e46f2bcb0a9ba7c3d506f6b477de2fdd1d1bc49b
Artem Naumov <artem.naumov.dev@gmail.com>
2025-06-30 17:55:09 +0300

    fix: SIGN_IN_TAG value space changed to underline

--- core/ui/src/main/java/com/greatwolf/ui/util/Const.kt
+++ core/ui/src/main/java/com/greatwolf/ui/util/Const.kt
@@ -4,4 +4,4 @@ const val ERROR_SNACKBAR_HOST_STATE_PROVIDE = "SnackbarHostState is not provided
 
 const val PRIVACY_TAG = "PRIVACY"
 const val TERMS_TAG = "TERMS"
-const val SIGN_IN_TAG = "SIGN IN"
+const val SIGN_IN_TAG = "SIGN_IN"
