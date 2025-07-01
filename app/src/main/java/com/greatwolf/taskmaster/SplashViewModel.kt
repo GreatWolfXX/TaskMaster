a670cea1705ea4e3077cd013e8a7532a0752b804
Artem Naumov <artem.naumov.dev@gmail.com>
2025-07-01 14:30:14 +0300

    feat: now splash scree use viewmodel for configure animation and choosing next destination

--- app/src/main/java/com/greatwolf/taskmaster/SplashViewModel.kt
+++ app/src/main/java/com/greatwolf/taskmaster/SplashViewModel.kt
@@ -6,24 +6,50 @@ import com.greatwolf.common.Result
 import com.greatwolf.common.asResult
 import com.greatwolf.domain.repository.SettingsRepository
 import com.greatwolf.taskmaster.navigation.Route
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+data class SplashUiState(
+    val progress: Float = 0f,
+    val destination: Route = Route.Onboarding
+)
+
+sealed class SplashEvent {
+    data object Idle : SplashEvent()
+    data object Finish : SplashEvent()
+}
+
+const val SPLASH_DELAY = 3000L
 
 class SplashViewModel(
     private val settingsRepository: SettingsRepository
 ) : ViewModel() {
 
-    val _destination = MutableStateFlow<Route>(Route.Onboarding)
-    val destination: StateFlow<Route> = _destination.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5000),
-        initialValue = Route.Onboarding
-    )
+    private var _event: Channel<SplashEvent> = Channel()
+    val event = _event.receiveAsFlow()
 
-    fun getDestination() {
+    private val _state = MutableStateFlow<SplashUiState>(SplashUiState())
+    val state: StateFlow<SplashUiState> = _state
+        .onStart {
+            startSplashLoading()
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = SplashUiState()
+        )
+
+    private fun getDestination() {
         settingsRepository.isOnboardingCompleted()
             .asResult()
             .map { result ->
@@ -31,12 +57,20 @@ class SplashViewModel(
                     is Result.Error -> {}
                     Result.Loading -> {}
                     is Result.Success -> {
+                        _state.update { it.copy(progress = 1f) }
                         if (result.data) {
-//                            _destination.update {  }
+                            _state.update { it.copy(destination = Route.SignUp) }
                         }
                     }
                 }
-            }
+            }.launchIn(viewModelScope)
     }
 
+    private fun startSplashLoading() {
+        viewModelScope.launch {
+            getDestination()
+            delay(SPLASH_DELAY)
+            _event.send(SplashEvent.Finish)
+        }
+    }
 }
