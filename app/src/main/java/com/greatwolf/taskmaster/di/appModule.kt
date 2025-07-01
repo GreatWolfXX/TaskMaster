19c2b83fd9033a6dd6ce3bc60a589bd1a9edd29f
Artem Naumov <artem.naumov.dev@gmail.com>
2025-07-01 14:32:42 +0300

    fix: add SplashViewModel to appModule

--- app/src/main/java/com/greatwolf/taskmaster/di/appModule.kt
+++ app/src/main/java/com/greatwolf/taskmaster/di/appModule.kt
@@ -1,7 +1,9 @@
 package com.greatwolf.taskmaster.di
 
+import com.greatwolf.taskmaster.SplashViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-
+    viewModelOf(::SplashViewModel)
 }
