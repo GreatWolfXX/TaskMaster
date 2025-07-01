be0b38eabcd4693b09615824f19090a507369be2
Artem Naumov <artem.naumov.dev@gmail.com>
2025-07-01 14:34:48 +0300

    fix: added inner padding

--- app/src/main/java/com/greatwolf/taskmaster/MainActivity.kt
+++ app/src/main/java/com/greatwolf/taskmaster/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.CompositionLocalProvider
@@ -15,6 +17,7 @@ import com.greatwolf.ui.theme.TaskMasterTheme
 import com.greatwolf.ui.util.LocalSnackbarHostState
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,7 +31,11 @@ class MainActivity : ComponentActivity() {
                     )
                 ) {
                     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        BasicNavigation()
+                        Box(
+                            modifier = Modifier.padding(innerPadding)
+                        ) {
+                            BasicNavigation()
+                        }
                     }
                 }
             }
