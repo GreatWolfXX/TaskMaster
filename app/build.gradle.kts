95b7bd589e16be3197f41f4e68769ca3c693efdc
Artem <artem.naumov.dev@gmail.com>
2025-07-22 14:06:33 +0300

    chore: update version to v0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.greatwolf.taskmaster"
         versionCode = 1
-        versionName = "0.7.0"
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
