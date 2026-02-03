8a630330d3df002e7ad14d5da440c69191ed1200
Artem <artem.naumov.dev@gmail.com>
2026-02-03 19:07:54 +0100

    feat: add welcome block

--- feature/home/src/main/java/com/greatwolf/home/HomeScreen.kt
+++ feature/home/src/main/java/com/greatwolf/home/HomeScreen.kt
@@ -1,10 +1,36 @@
 package com.greatwolf.home
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.greatwolf.ui.component.CustomIconButton
+import com.greatwolf.ui.theme.BodyXSmallTextStyleNormal
+import com.greatwolf.ui.theme.Neutral200
+import com.greatwolf.ui.theme.Neutral50
+import com.greatwolf.ui.theme.Neutral500
+import com.greatwolf.ui.theme.Neutral700
+import com.greatwolf.ui.theme.Typography
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -27,7 +53,51 @@ private fun HomeContent(
     state: HomeUiState,
     onIntent: (HomeIntent) -> Unit,
 ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(horizontal = 16.dp, vertical = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        WelcomeBlock(
+            name = "Artem", //TEST
+            onClickNotification = {}
+        )
+    }
+}
 
+@Composable
+private fun WelcomeBlock(
+    name: String,
+    onClickNotification: () -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            Modifier.wrapContentSize()
+        ) {
+            Text(
+                modifier = Modifier,
+                text = stringResource(R.string.welcome_back, name),
+                style = BodyXSmallTextStyleNormal,
+                color = if (isSystemInDarkTheme()) Neutral200 else Neutral500
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(
+                modifier = Modifier,
+                text = stringResource(R.string.ready_conquer_day),
+                style = Typography.labelMedium,
+                color = if (isSystemInDarkTheme()) Neutral50 else Neutral700
+            )
+        }
+        CustomIconButton(
+            icon = ImageVector.vectorResource(com.greatwolf.ui.R.drawable.ic_notification),
+            onClick = onClickNotification
+        )
+    }
 }
 
 @Preview
